1c16f529c2d0233c07cdb430e71c1763444f8330
ajb73100 <62798258+ajb73100@users.noreply.github.com>
2020-04-17 23:28:37 -0400

    Add files via upload
    this is the python script for finding a specific version by its date

---
+++ pythondate2.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import argparse
+import urllib.request
+
+
+domain = 'https://www.python.org'
+url = 'https://www.python.org/downloads'
+
+parser = argparse.ArgumentParser(description = "this program is for finding versions of python based on there release dates")
+
+parser.add_argument('-r', required=False, metavar= "date", type=str, help="release date, capatilize first letter of each month, day, then year. example: April 6, 2013")
+args = parser.parse_args()
+print (args.r)
+
+args.r = "April 6, 2013"
+
+versions = []
+
+def get_soup(url):
+    request = requests.get(url)
+    return BeautifulSoup(requests.get(url).text, 'html.parser')
+soup = get_soup(url)
+
+for link in soup.select('.list-row-container li'):
+    string = str(link.prettify())
+    if (args.r) in string:
+
+        splitstring = string.split()
+        versions.append(splitstring[6])
+        #print (splitstring[6])
+print (versions[0])
+download = "https://www.python.org/ftp/python/"+versions[0]+"/Python-"+versions[0]+".tgz"
+
+
+urllib.request.urlretrieve(download, 'abennettpython'+versions[0]+".tgz")
+
